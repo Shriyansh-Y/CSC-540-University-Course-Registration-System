65494d6e055d59b172e102c8670a646cb423f4aa
asmotiwale <asmotiwale2194@gmail.com>
2017-03-27 09:21:51 -0400

    added code for view profile for admin

--- CourseRegistration/src/javaCode/AdminView.java
+++ CourseRegistration/src/javaCode/AdminView.java
@@ -16,7 +16,22 @@ public class AdminView {
 	
 	// Method to view admin profile
 	public static void viewProfile(Scanner ip){
+		while(true){
+			System.out.println("**Your Profile**");
+			System.out.println("Press 0 to go back.");
+			System.out.println("First Name: " + AdminProfile.getInstance().getFirstname());
+			System.out.println("Last Name: " + AdminProfile.getInstance().getLastname());
+			System.out.println("Date of Birth: " + AdminProfile.getInstance().getDob());
+			System.out.println("Employee id: " + AdminProfile.getInstance().getEid());
 		
+			int n = ip.nextInt();
+			if(n == 0){
+				Login.admin_homepage(ip);
+			}
+			else{
+				System.out.println("Please enter 0 to go back.");
+			}
+		}
 	}
 	
 	// Method to view/add courses
