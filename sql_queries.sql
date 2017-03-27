e18011646c29b7f8f9a7b53045c67407eaeb94cc
asmotiwale <asmotiwale2194@gmail.com>
2017-03-27 02:21:14 -0400

    Updated the sql queries

--- sql_queries.sql
+++ sql_queries.sql
@@ -1,5 +1,5 @@
 DROP TABLE Student;
-DROP TABLE Administrator;
+DROP TABLE Admins;
 
 CREATE TABLE Student(
   Student_id INT,
@@ -14,12 +14,25 @@ CREATE TABLE Student(
   CONSTRAINT student_pk PRIMARY KEY(Student_id)
 );
 
-CREATE TABLE Administrator(
+CREATE TABLE Admins(
   Employee_id INT,
   psswd VARCHAR(50) NOT NULL,
-  SSN INT NOT NULL,
+  SSN VARCHAR(7) NOT NULL,
   First_Name VARCHAR(20) NOT NULL,
   Last_Name VARCHAR(20) NOT NULL,
   Dateofbirth DATE NOT NULL,
   CONSTRAINT admin_pk PRIMARY KEY(Employee_id)
 );
+
+SELECT * FROM Student;
+INSERT INTO Student
+VALUES (100 ,'secret', 'Anuraag', 'Motiwale', 'asmotiwa@ncsu.edu',1, 2, 0.0, TO_DATE('1989-12-09','YYYY-MM-DD'));
+
+SELECT * FROM Admins;
+INSERT INTO Admins
+VALUES(101, 'admin1', 1234567, 'Abhishek', 'Singh', TO_DATE('1989-12-09','YYYY-MM-DD'));
+
+INSERT INTO Admins
+VALUES(102, 'admin2', 1234567, 'Parag', 'Nakhwa', TO_DATE('1989-12-09','YYYY-MM-DD'));
+
+
