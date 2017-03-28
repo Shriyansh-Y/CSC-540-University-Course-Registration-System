c276e18ac732c29ea9bafde1e9922375c5757c75
asmotiwale <asmotiwale2194@gmail.com>
2017-03-28 13:16:29 -0400

    Added code for viewing and editting courses

--- CourseRegistration/src/javaCode/Queries.java
+++ CourseRegistration/src/javaCode/Queries.java
@@ -6,7 +6,6 @@ public final class Queries {
 	static final String verify_login_admin = "Select * from Admins where username = ? and psswd = ?";
 	static final String enroll_new_student = "Insert into Student Values(?,?,?,?,?,?,?,?,?,?,?,TO_DATE(?,'YYYY-MM-DD'))";
 	static final String get_student_details = "Select * from Student Where Student_id = ?";
-
 	static final String add_course_offerings = "Insert into course_offering values(?,?,?,?,TO_DSINTERVAL(?),TO_DSINTERVAL(?),?,?,?)";
 	static final String verify_course_offering = "Select * from course_offering where COURSE_ID = ? and FACULTY_NAME = ?";
 	static final String view_course_offerings = "Select * from course_offering where COURSE_ID = ?";
@@ -18,4 +17,6 @@ public final class Queries {
 	static final String verify_update_student_lastname = "Select * From Student Where Last_Name = ? and Student_id = ?";
 	static final String verify_update_student_phone = "Select * From Student Where phone = ? and Student_id = ?";
 	static final String verify_update_student_email = "Select * From Student Where email = ? and Student_id = ?";	
+	static final String add_new_course= "Insert into courses values(?,?,?,?,?,?,?,?)";
+	static final String view_course= "Select * from courses where course_id = ?";
 }
